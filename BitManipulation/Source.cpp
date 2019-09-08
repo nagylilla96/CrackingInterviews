e406394b2f109cdd220902ed5d61d13eec868561
nagylilla96 <n.lilla96@gmail.com>
2019-09-08 22:33:17 +0300

    Added solution to exercise 3 from bit manipulation

--- BitManipulation/Source.cpp
+++ BitManipulation/Source.cpp
@@ -49,11 +49,60 @@ string exercise2(double num) {
 	return number;
 }
 
+int bitsSet(int n) {
+	int bits = 0;
+
+	while (n != 0) {
+		n = n >> 1;
+		bits++;
+	}
+
+	return bits;
+}
+
+int fitWindow(int w, int n) {
+	int amount = 0;
+	while (n != 0) {
+		n = n >> 1;
+		amount++;
+	}
+	w = 1 << (amount - 1);
+	return w;
+}
+
+// It's a bit overcomplicated, I could have started on the right, but oh well :))
+int exercise3(int n) {
+	int window = fitWindow((1 << 31) | 0, n);
+	int left = 0, right = 0;
+	int occur = 0;
+	int index = 0;
+	int max = 0;
+
+	while (window != 0) {
+		if ((n & window) != window) {
+			if (occur == 0) {
+				index = right;
+				occur++;
+			}
+			else {
+				if (right - left > max) max = (right - left);
+				left = index + 1;
+				index = right;
+			}
+		}
+		right++;
+		window = window >> 1;
+	}
+	if (right - left > max) return (right - left);
+	return max;
+}
+
 int main() {
 	int n = 1024;
 	if (!exercise1(n, 19, 2, 6)) cout << "1. FALSE";
 	else cout << "1. " << n;
 	cout << "\n2. " << exercise2(0.625) << endl;
+	cout << "3. " << exercise3(1775) << endl;
 	getchar();
 	return 0;
 }
