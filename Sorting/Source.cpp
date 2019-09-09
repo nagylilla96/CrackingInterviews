92b92323b7de880320ac422c1cd0385997b127d0
nagylilla96 <n.lilla96@gmail.com>
2019-09-09 20:02:28 +0300

    Added better solution for 10.1

--- Sorting/Source.cpp
+++ Sorting/Source.cpp
@@ -5,27 +5,24 @@ using namespace std;
 // Chapter 10 - woo!
 
 // Exercise 10.1
-void merge(vector<int>& a, vector<int> &b) {
-	int indA = 0;
-	int indB = 0;
+void merge(int* a, int* b, int sizeA, int sizeB) {
+	int indA = sizeA - 1;
+	int indB = sizeB - 1;
+	int indLast = sizeA + sizeB - 1;
 
-	while (indA != a.size()) {
+	while (indB >= 0) {
 		if (b[indB] < a[indA]) {
-			a.insert(a.begin() + indA, b[indB]);
-			indB++;
+			a[indLast--] = a[indA--];
 		}
-		indA++;
-	}
-
-	while (indB != b.size()) {
-		a.insert(a.end(), b[indB]);
-		indB++;
+		else {
+			a[indLast--] = b[indB--];
+		}
 	}
 }
 
-void print(vector<int> &a) {
-	for (auto x : a) {
-		cout << x << " ";
+void print(int* a, int sizeA) {
+	for (int i = 0; i < sizeA; i++) {
+		cout << a[i] << " ";
 	}
 	cout << endl;
 }
@@ -33,10 +30,16 @@ void print(vector<int> &a) {
 int main() {
 	int a[4] = { 1, 2, 3, 5 };
 	int b[5] = { 1, 2, 4, 8, 9 };
-	vector<int> vecA(a, a + 4);
-	vector<int> vecB(b, b + 5);
 
-	merge(vecA, vecB);
-	print(vecA);
+	int* vecA = (int*)malloc(10 * sizeof(int));
+	for (int i = 0; i < 4; i++) {
+		vecA[i] = a[i];
+	}
+	int* vecB = (int*)malloc(5 * sizeof(int));
+	for (int i = 0; i < 5; i++) {
+		vecB[i] = b[i];
+	}
+	merge(vecA, vecB, 4, 5);
+	print(vecA, 9);
 	getchar();
 }
